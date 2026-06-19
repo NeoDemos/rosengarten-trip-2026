afd1f834857f3521cfd750b2c2289489c74d11dd
dennis tak <tak.dpa@gmail.com>
2026-06-19 11:17:51 +0200

    Update Rosengarten trip page

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "rosengarten-trip-shell-v4";
+const CACHE_NAME = "rosengarten-trip-shell-v5";
 const RUNTIME_CACHE = "rosengarten-trip-runtime-v1";
 const SHELL_ASSETS = [
   "./",
